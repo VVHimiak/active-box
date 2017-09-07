413ceb99c28e77cb70931a6201b41e1eae59bdd3
Vladimir Himiak <vv.himiak@gmail.com>
2017-09-07 20:46:15 +0300

    commit-10

--- js/main.js
+++ js/main.js
@@ -1,23 +1,8 @@
-window.onload = function () {
-
-
-   var left = document.querySelector('.left');
-   var right = document.querySelector('.right');
-
-   function equalHeight() {
-       left.style.height = 'auto';
-       right.style.height = 'auto';
-
-       var leftH = left.offsetHeight;
-       var rightH = right.offsetHeight;
-
-       var max = Math.max(leftH, rightH);
-
-       left.style.height = max + 'px';
-       right.style.height = max + 'px';
-       console.log(left);
-   }
-   equalHeight();
-
-   window.onresize = equalHeight();
-};
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 2,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
+});
